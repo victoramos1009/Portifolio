26322b01f6c3edff9d87341c77a4e7f8ae8635ea
victor pereira <victoramos1009@yahoo.com.br>
2023-01-04 14:45:25 -0300

    tentando arrumar

---
+++ JavaScript/script.js
@@ -0,0 +1,3 @@
+function btnlinkedin() {
+    const win = Window.open("https://www.linkedin.com/in/victor-p-17138112a/, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=400");
+} 
